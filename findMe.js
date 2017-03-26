9e29c15b6715c0729a6a134d2893ce84c1c09668
Alsidneio <alsidneio@gmail.com>
2017-03-25 17:11:09 -0700

    fixed the return problem I was having

--- findMe.js
+++ findMe.js
@@ -1,33 +1,34 @@
 function findMe(num,set){
+var ans;
 if(num<set[0]||num>set[set.length-1]){
   console.log('not even in range');
-  return false;
+  ans=false;
 }
 
 function search(num,set){
   var check=Math.ceil((last-start)/2);
   if(check==0){
-    console.log('not here');
-    return false;
+    ans=false;
+    return;
   }
   if(num>set[start+check]){
       start=start+check;
-      console.log('gotta recurse');
       search(num,set)
     }
   else if(num<set[start+check]){
     last=last-check;
-    console.log('gotta recurse');
     search(num,set)
   }
   else{
-    console.log('im here')
-    return true;
+    ans=true;
+    return;
   }
 }
 
 var start=0;
 var last=set.length-1;
+search(num,set)
+return ans;
 }
 
 test=[0,2,6,8,10,16,20,32,90];
